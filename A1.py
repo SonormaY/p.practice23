5fb3d1715f561cc6a736a976663b73384599068b
Stanislav <144832051+SonormaY@users.noreply.github.com>
2023-09-13 19:47:41 +0300

    Added function to count ways for rabbit

--- A1.py
+++ A1.py
@@ -1,3 +1,13 @@
+import math
+
+def count(n,k):
+    if n < k:
+        return 0
+    
+
+    # C(n, k) = (n + k - 1)! / k! * (n - 1)!
+    return math.factorial(n + k - 1) // math.factorial(k) * math.factorial(n - 1)
+
 
 
 
