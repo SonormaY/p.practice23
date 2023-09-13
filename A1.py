a8cdc1a048d7043353698e021f93dca558c6a1d7
Stanislav <144832051+SonormaY@users.noreply.github.com>
2023-09-13 19:56:44 +0300

    Added main menu for program

--- A1.py
+++ A1.py
@@ -4,9 +4,25 @@ def count(n,k):
     if n < k:
         return 0
     
-
     # C(n, k) = (n + k - 1)! / k! * (n - 1)!
     return math.factorial(n + k - 1) // math.factorial(k) * math.factorial(n - 1)
+
+while True:
+    #input of data
+    k = input("Enter k: ")
+    n = input("Enter n: ")
+
+    # basic check for input
+    if k.isdigit() and n.isdigit():
+        k = int(k)
+        n = int(n)
+    else:
+        print("You must enter digits to continue!!!")
+        continue
+    
+    # printing result
+    print("Number of ways rabbit can jump ladder is: " + str(count(n, k)))
+
 
 
 
