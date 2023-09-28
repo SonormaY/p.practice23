e21fa00f8de1d0b0c1e495adfd57e617d13f1914
Stanislav <144832051+SonormaY@users.noreply.github.com>
2023-09-28 19:36:56 +0300

    Fixed validation problem

--- A2/A2.py
+++ A2/A2.py
@@ -2,16 +2,20 @@ from A2.linked_list import LinkedList
 
 def do_some_magic2():
     ll = LinkedList()
-    choice = int(input("Enter 1 to input list manually or 2 to generate randomly: "))
-    if choice == 1:
-        ll.input()
-    elif choice == 2:
-        a = int(input("Enter a: "))
-        b = int(input("Enter b: "))
-        amount = int(input("Enter amount to generate: "))
-        ll.randomFill(amount, a, b)
-        print("LinkedList:")
-        ll.print()
+    choice = input("Enter 1 to input list manually or 2 to generate randomly: ")
+    while True:
+        if choice == '1':
+            ll.input()
+            break
+        elif choice == '2':
+            a = int(input("Enter a: "))
+            b = int(input("Enter b: "))
+            amount = int(input("Enter amount to generate: "))
+            ll.randomFill(amount, a, b)
+            print("LinkedList:")
+            ll.print()
+            break
+        else: print("Enter a valid number")
     ll.reverseNegativeNumbers()
     print()
     ll.print()
