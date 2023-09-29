5441d14d777ad4a9074d2bccefa7b5cd790222a9
Stanislav <144832051+SonormaY@users.noreply.github.com>
2023-09-29 10:50:24 +0300

    fixed validation

--- A2/A2.py
+++ A2/A2.py
@@ -1,21 +1,36 @@
 from A2.linked_list import LinkedList
 
+def validate_positive_int(n):
+    return n.isdigit() and int(n) > 0
+
+def validate_int(n):
+    if n[0] == '-':
+        return n[1:].isdigit()
+    return n.isdigit()
+
+
+
 def do_some_magic2():
-    ll = LinkedList()
-    choice = input("Enter 1 to input list manually or 2 to generate randomly: ")
-    while True:
-        if choice == '1':
-            ll.input()
-            break
-        elif choice == '2':
-            a = int(input("Enter a: "))
-            b = int(input("Enter b: "))
-            amount = int(input("Enter amount to generate: "))
-            ll.randomFill(amount, a, b)
-            print("LinkedList:")
-            ll.print()
-            break
-        else: print("Enter a valid number")
-    ll.reverseNegativeNumbers()
-    print()
-    ll.print()
+    while exit:
+        ll = LinkedList()
+        while True:
+            choice = input("Enter 1 to input list manually or 2 to generate randomly. Enter 'e' to exit: ")
+            if choice == '1':
+                ll.input()
+                break
+            elif choice == '2':
+                a = input("Enter a: ")
+                b = input("Enter b: ")
+                amount = input("Enter amount to generate: ")
+                if validate_int(a) and validate_int(b) and validate_positive_int(amount):
+                    ll.randomFill(int(amount), int(a), int(b))
+                    print("LinkedList:")
+                    ll.print()
+                else: print("Error in input data!!!")
+                break
+            elif choice == 'e':
+                return
+            else: print("Enter a valid number")
+        ll.reverseNegativeNumbers()
+        print()
+        ll.print()
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from A2.A2 import do_some_magic2
 from A2.linked_list import LinkedList
 if __name__ == "__main__":
     print("Menu: ")
-    print("№2 LinkedList && ReverseNegativeNumbers")
-    user_input = int(input("Select № of Assignment to execute:"))
+    while True:
+        print("№2 LinkedList && ReverseNegativeNumbers")
+        user_input = input("Select № of Assignment to execute:")
 
-    if user_input == 2:
-        do_some_magic2()
+        if user_input == '2':
+            do_some_magic2()
+        else: print("Enter valid number")
