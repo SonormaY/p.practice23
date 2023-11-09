20c5b3a4d1817b244eb0efc41eafa8eb45906aee
Stanislav <144832051+SonormaY@users.noreply.github.com>
2023-11-09 17:02:25 +0200

    init branch

---
+++ A5/Vaccine.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Vaccine(Enum):
+    PFIZER = "pfizer"
+    MODERNA = "moderna"
+    ASTRA_ZENECA = "AstraZeneca"
--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-if __name__ == "__main__":
-    print()
+from os import system, name
+from validation import Validator
+from A5.A5 import do_some_magic5
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+while True:
+    clear()
+    while True:
+        user_input = input("Enter number to execute (enter 0 to exit): ")
+        if Validator.is_integer(user_input):
+            break
+        else: print("Enter a number")
+    if user_input == "4":
+        do_some_magic5()
+        print()
+        input("Press enter to continue...")
+    elif user_input == "0": break
+    else: 
+        print("Enter valid number of program")
+        input("Press enter to continue...")
